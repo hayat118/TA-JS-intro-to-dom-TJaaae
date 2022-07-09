28b1f27607bc8d89ab39b607b94cc48794c13c5b
ravi11o <ravi.suraj110@gmail.com>
2022-07-09 08:54:17 +0530

    people of Got

---
+++ block-BJaaef/code/script.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+let ul=document.querySelector("ul");
+
+got.houses.forEach((people)=>{
+  let li=document.createElement("li");
+  let img=document.createElement("img");
+  img.src=people.image;
+  img.alt=people.name;
+  let h2=document.createElement("h2");
+  h2.innerText=people.name;
+  let p=document.createElement("p");
+  p.innerText=people.description;
+  let button=document.createElement("button");
+  button.innerText="Learn More";
+  li.append(img,h2,p,button);
+  console.log(li);
+  // ul.append(li);
+
+})
