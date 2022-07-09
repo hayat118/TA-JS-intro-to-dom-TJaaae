1cce5a34c7be60b9c6e90f5ed722c6c168f3f7b9
ravi11o <ravi.suraj110@gmail.com>
2022-07-09 16:59:15 +0530

    dynamic clock

--- block-BJaaeh/code/script.js
+++ block-BJaaeh/code/script.js
@@ -1,29 +1,44 @@
- setInterval(showTime, 1000);
-        function showTime() {
-            let time = new Date();
-            let hour = time.getHours();
-            let min = time.getMinutes();
-            let sec = time.getSeconds();
-            am_pm = "AM";
+//  setInterval(showTime, 1000);
+//         function showTime() {
+//             let time = new Date();
+//             let hour = time.getHours();
+//             let min = time.getMinutes();
+//             let sec = time.getSeconds();
+//             am_pm = "AM";
  
-            if (hour > 12) {
-                hour -= 12;
-                am_pm = "PM";
-            }
-            if (hour == 0) {
-                hr = 12;
-                am_pm = "AM";
-            }
+//             if (hour > 12) {
+//                 hour -= 12;
+//                 am_pm = "PM";
+//             }
+//             if (hour == 0) {
+//                 hr = 12;
+//                 am_pm = "AM";
+//             }
  
-            hour = hour < 10 ? "0" + hour : hour;
-            min = min < 10 ? "0" + min : min;
-            sec = sec < 10 ? "0" + sec : sec;
+//             hour = hour < 10 ? "0" + hour : hour;
+//             min = min < 10 ? "0" + min : min;
+//             sec = sec < 10 ? "0" + sec : sec;
  
-            let currentTime = hour + ":"
-                + min + ":" + sec + am_pm;
+//             let currentTime = hour + ":"
+//                 + min + ":" + sec + am_pm;
  
-            document.getElementById("clock")
-                // .innerHTML = currentTime;
-        }
+//             document.getElementById("clock")
+//                 .innerHTML = currentTime;
+//         }
  
-        showTime();
+//         showTime();
+
+
+
+function clockInit(){
+    var date = new Date();
+    var time = [date.getHours(), date.getMinutes(), date.getSeconds()];
+    var clockDivs = [document.getElementById("hour"), document.getElementById("min"), document.getElementById("sec")];
+    
+    var hour = time[1]/2+time[0]*30;
+    
+    clockDivs[0].style.transform="rotate("+ hour +"deg)";
+    clockDivs[1].style.transform="rotate("+ time[1]*6 +"deg)";
+    clockDivs[2].style.transform="rotate("+ time[2]*6 +"deg)";
+}
+clockInit();
